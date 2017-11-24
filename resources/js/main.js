f38278e69db6e11cdaf4651d9cae127e3dc600be
Anuscke Botha <anusckeb@gmail.com>
2017-11-24 14:34:52 +0200

    fixed and updated

--- resources/js/main.js
+++ resources/js/main.js
@@ -17,7 +17,7 @@ $(window) .scroll(function(){
 		var navHeight = $(".navbar").css("height");
 		$(".navbar").animate({top: "-" + navHeight}, 150);
 		lastScrollTop = scrollTop; 
-	} else if (lastScrollTop -nscrollTop > 50) {
+	} else if (lastScrollTop -scrollTop > 50) {
 		$(".navbar") .animate({top: "0px"}, 150);
 		lastScrollTop = scrollTop;
 	}
